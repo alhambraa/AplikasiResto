e7d4742582f31cd9077feb045c07ce0a8606a8a0
alhambraa <hambra02@gmail.com>
2022-12-07 20:02:36 +0700

    Update MenuSpecial.java
    PR Check Al Hambra

--- src/MenuSpecial.java
+++ src/MenuSpecial.java
@@ -1,4 +1,8 @@
 public class MenuSpecial {
+    public static void main(String[] args) {
     String nameMenu = "";
     Integer priceMenu = 0;
+
+    System.out.println("Aplikasi Resto");
+    }
 }
